e32a34bc3c561d7f02fe4819841ed08efcd0861a
chao-hsiao <joy510224.ch@gmail.com>
2021-03-25 03:46:35 +0300

    run.c

--- run.c
+++ run.c
@@ -1,3 +1,4 @@
+/*
 #include "pdp11.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -84,4 +85,109 @@ void do_nothing() {
 void trace(char * c, word pc, word w) {
 	printf(c, pc, w);
 }
+*/
+#include "pdp11.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
+#define pc reg[7]
+
+void run();
+void do_halt();
+void do_nothing();
+void do_mov();
+void do_add();
+//void do_inc();
+
+typedef struct {
+	word mask;
+	word opcode;
+	char * name;
+	void (*do_func)();
+} Command;
+
+Command command[] = {
+	{0170000, 0010000, "mov", do_mov},
+	{0170000, 0060000, "add", do_add},
+	{0177777, 0000000, "halt", do_halt},
+	//{0177700, 0005200, "inc", do_inc}
+	{0000000, 0000000, "nothing", do_nothing} // MUST LAST
+};
+
+//enum LOGLEVEL current_log_level = INFO;
+int current_log_level = DEBUG;
+
+void usage(const char * filename) {
+	printf("USAGE: %s pdp_filename\n", filename);
+}
+
+int main(int argc, char const *argv[]) {
+
+	if (argc <= 1) {
+		usage(argv[0]);
+		return 1;
+	}
+	load_file(argv[1]);
+	mem_dump(0x200,0xc);
+	run();
+	/*
+	int x = 4;
+    trace(DEBUG, "My debug info x=%d\n", x);
+	x = 6;
+    trace(TRACE, "trace x=%d\n", x);
+	trace(INFO, "hello\n");
+	trace(ERROR, "ERROR: %s\n", command[0].name);
+	int old_log_level = current_log_level;
+	current_log_level = DEBUG;
+    trace(DEBUG, "My debug info x=%d\n", x);
+	current_log_level = old_log_level;
+	*/
+	return 0;
+}
+
+void run() {
+	pc = 01000;
+
+	while(1) {
+		word w = w_read(pc);
+		trace(TRACE, "%06o %06o: ", pc, w);
+		pc = pc + 2;
+
+		int i;
+		for(i = 0; ; i++) {
+			Command cmd = command[i];
+			if ((w & cmd.mask) == cmd.opcode) {
+				trace(TRACE, "%s ", cmd.name);
+				cmd.do_func();
+				trace(TRACE, "\n");
+				break;
+			}
+		}
+	}
+}
+
+void do_add() {
+}
+
+void do_mov() {
+}
+
+void do_halt() {
+	printf("THE END\n");
+	exit(0);
+}
+
+void do_nothing() {
+  
+}
+
+void trace(int log_level, const char * format, ...){
+	// никакой печати, если логирование ниже по уровню
+	if (log_level < current_log_level)
+		return;
+	va_list ap;
+	va_start(ap, format);
+	vprintf(format, ap);
+	va_end(ap);
+}
